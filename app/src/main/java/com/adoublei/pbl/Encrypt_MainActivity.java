1c113669eb1772e3acc515cb74ef4093b35a9020
MinYoung-Lim <minyounge17@naver.com>
2020-07-14 13:47:33 +0900

    OpenCV, 암호화 버튼 추가 -> 화면전환 가능하게 함

---
+++ app/src/main/java/com/adoublei/pbl/Encrypt_MainActivity.java
@@ -0,0 +1,14 @@
+package com.adoublei.pbl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Encrypt_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encrypt__main);
+    }
+}
--- app/src/main/java/com/adoublei/pbl/MainActivity.java
+++ app/src/main/java/com/adoublei/pbl/MainActivity.java
@@ -2,13 +2,40 @@ package com.adoublei.pbl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_opencv;
+    Button btn_encrypt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_opencv = findViewById(R.id.btn_opencv);
+        btn_encrypt = findViewById(R.id.btn_encrypt);
+
+        btn_opencv.setOnClickListener(new View.OnClickListener() {  // OpenCV화면으로 이동
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Opencv_MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_encrypt.setOnClickListener(new View.OnClickListener() {  // 암호화화면으로 이동
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Encrypt_MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
